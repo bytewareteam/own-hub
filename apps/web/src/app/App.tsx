6ddb1f2ce76cba935c2d36bb24067f12243d263b
Willian Monoga <bytewareteam@gmail.com>
2023-04-18 19:28:14 -0500

    feat(web): add login button (#1)

--- apps/web/src/app/App.tsx
+++ apps/web/src/app/App.tsx
@@ -1,9 +1,12 @@
 export function App() {
   return (
-    <h1>
-      <span> Hello there, </span>
-      Welcome OwnHub 👋
-    </h1>
+    <>
+      <h1>
+        <span> Hello there, </span>
+        Welcome OwnHub 👋
+      </h1>
+      <button>Sign in</button>
+    </>
   );
 }
 
